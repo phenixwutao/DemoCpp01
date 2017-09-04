4f76903d7ffcd5d9265507efe4f5a8f4c5d8e8b5
phenix wutao <phenixwutao@gmail.com>
2017-09-04 14:14:53 +0100

    39 - chap03 Thread Unsafe Shared Data

--- DemoCppMultithreading/chap03/chap03ThreadSharingData.cpp
+++ DemoCppMultithreading/chap03/chap03ThreadSharingData.cpp
@@ -26,6 +26,47 @@ bool list_contains(int value_to_find)
 void chap03TestMutexLock()
 {
   add_to_list(42);
-  std::cout << "contains(1)=" << list_contains(1) 
-            << ", contains(42)=" << list_contains(42) << std::endl;
+  std::cout << "contains(1)=" << list_contains(1)
+    << ", contains(42)=" << list_contains(42) << std::endl;
+}
+
+/********************************
+* Test unsafe share data 
+********************************/
+class some_data
+{
+  int a;
+  std::string b;
+public:
+  void do_something()
+  {}
+};
+
+class data_wrapper
+{
+private:
+  some_data data;
+  std::mutex m;
+public:
+  template<typename Function>
+  void process_data(Function func)
+  {
+    std::lock_guard<std::mutex> l(m);
+    func(data); // 1 pass mutex lock protected data
+  }
+};
+
+some_data* unprotected;
+
+void malicious_function(some_data& protected_data)
+{
+  unprotected = &protected_data;
+}
+
+
+void chap03TestUnsafeSharedData()
+{
+  data_wrapper x;
+  x.process_data(malicious_function); // 2 pass a malicious function using ptotected data reference or pointer
+  unprotected->do_something(); // 3 do something without mutex lock protection
 }
--- DemoCppMultithreading/chap03/chap03ThreadSharingData.h
+++ DemoCppMultithreading/chap03/chap03ThreadSharingData.h
@@ -1,3 +1,4 @@
 #pragma once
 
 void chap03TestMutexLock();
+void chap03TestUnsafeSharedData();
