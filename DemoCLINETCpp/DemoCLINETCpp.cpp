5cc9825cf8a4f76962ef9908168ff7b29e77221f
phenix wutao <phenixwutao@gmail.com>
2017-09-29 00:51:17 +0100

    111 - Demo CLI Change Console Color

--- DemoCLINETCpp/DemoCLINETCpp.cpp
+++ DemoCLINETCpp/DemoCLINETCpp.cpp
@@ -1,11 +1,24 @@
 // DemoCLINETCpp.cpp : main project file.
 
 #include "stdafx.h"
+#include <iostream>
 
 using namespace System;
 
 int main(array<System::String ^> ^args)
 {
-    Console::WriteLine(L"Hello World");
-    return 0;
+  Console::ForegroundColor = ConsoleColor::Cyan; // normal display color
+  Console::WriteLine(L"Hello World");
+  Console::ResetColor();
+
+  Console::ForegroundColor = ConsoleColor::Magenta; // error color
+  Console::WriteLine(L"Hello World");
+  Console::ResetColor();
+
+  Console::ForegroundColor = ConsoleColor::Yellow; // highlight or warning color
+  Console::WriteLine(L"Hello World");
+  Console::ResetColor();
+
+  system("pause");
+  return 0;
 }
