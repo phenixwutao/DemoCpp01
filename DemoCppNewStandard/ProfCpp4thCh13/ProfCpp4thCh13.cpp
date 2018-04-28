e0faf2bfceecc351eb3d5e54b88df7501afb435c
phenix wutao <phenixwutao@gmail.com>
2018-04-28 23:44:48 +0100

    325 - chap13 Demo Input Output With Objects

--- DemoCppNewStandard/ProfCpp4thCh13/ProfCpp4thCh13.cpp
+++ DemoCppNewStandard/ProfCpp4thCh13/ProfCpp4thCh13.cpp
@@ -5,6 +5,10 @@
 #include <locale>
 #include <iomanip>
 #include <stdexcept>
+#include <sstream>
+#include <string>
+#include <string_view>
+
 using namespace std;
 namespace chap13
 {
@@ -14,13 +18,13 @@ namespace chap13
   *cerr	An unbuffered output stream, writes data to the "error console," which is often the same as the "output console."
   *clog	A buffered version of cerr
   *
-  * The difference between buffered and unbuffered streams is that a buffered stream 
-  * does not immediately send the data to the destination, but instead, it buffers 
-  * incoming data and then sends it in blocks. An unbuffered stream, on the other hand, 
-  * immediately sends the data to the destination. Buffering is usually done to 
-  * improve performance, as certain destinations, such as files, perform better when 
-  * writing bigger blocks at once. Note that you can always force a buffered stream to 
-  * send all its currently buffered data to the destination by flushing its buffer 
+  * The difference between buffered and unbuffered streams is that a buffered stream
+  * does not immediately send the data to the destination, but instead, it buffers
+  * incoming data and then sends it in blocks. An unbuffered stream, on the other hand,
+  * immediately sends the data to the destination. Buffering is usually done to
+  * improve performance, as certain destinations, such as files, perform better when
+  * writing bigger blocks at once. Note that you can always force a buffered stream to
+  * send all its currently buffered data to the destination by flushing its buffer
   * using the flush() method
   *******************************************************************************************/
   void chap13DemoBasicStream()
@@ -44,7 +48,7 @@ namespace chap13
     {
       cerr << "Unable to flush to standard out" << endl;
     }
-    
+
     // Streams have a conversion operator to convert to type bool
     if (!cout)
     {
@@ -61,7 +65,7 @@ namespace chap13
     }
     catch (const ios_base::failure& ex) {
       cerr << "Caught exception: " << ex.what()
-           << ", error code = " << ex.code() << endl;
+        << ", error code = " << ex.code() << endl;
     }
   }
 
@@ -136,192 +140,275 @@ namespace chap13
     cout.precision(5);
     cout << "This should be '1.2346': " << 1.23456789 << endl;
   }
-}
 
-namespace DemoInput
-{
-  void MyInput()
+
+  namespace DemoInput
   {
-    string userInput;
-    cin >> userInput;
-    cout << "User input was " << userInput << endl;
-
-    int intInput;
-    cin >> intInput;
-    cout << "User input was " << intInput << endl;
-
-    string guestName;
-    int partySize;
-    cout << "Name and number of guests: ";
-    cin >> guestName >> partySize;
-    cout << "Thank you, " << guestName << "." << endl;
-    if (partySize > 10) {
-      cout << "An extra gratuity will apply." << endl;
-    }
-  }
-
-  void MyInputErrors()
-  {
-    cout << "Enter numbers on separate lines to add. "
-      << "Use Control+D to finish (Control+Z in Windows)." << endl;
-    int sum = 0;
-
-    if (!cin.good()) {
-      cerr << "Standard input is in a bad state!" << endl;
-      return;
-    }
-
-    int number;
-    while (!cin.bad()) {
-      cin >> number;
-      if (cin.good()) {
-        sum += number;
-      }
-      else if (cin.eof()) {
-        break; // Reached end of file
-      }
-      else if (cin.fail()) {
-        // Failure!
-        cin.clear(); // Clear the failure state.
-        string badToken;
-        cin >> badToken; // Consume the bad input.
-        cerr << "WARNING: Bad input encountered: " << badToken << endl;
-      }
-    }
-
-    cout << "The sum is " << sum << endl;
-  }
-
-
-  string readName1(istream& stream)
-  {
-    string name;
-    while (stream) { // Or: while (!stream.fail()) {
-      int next = stream.get();//returns the next character in the stream
-      if (!stream || next == std::char_traits<char>::eof())
-        break;
-      name += static_cast<char>(next);// Append character.
-    }
-    return name;
-  }
-
-
-  string readName2(istream& stream)
-  {
-    string name;
-    char next;
-    while (stream.get(next))
+    void MyInput()
     {
-      name += next;
-    }
+      string userInput;
+      cin >> userInput;
+      cout << "User input was " << userInput << endl;
 
-    return name;
-  }
+      int intInput;
+      cin >> intInput;
+      cout << "User input was " << intInput << endl;
 
-  /*****************************************************************************************
-  * A call to unget() causes the stream to back up by one position, essentially putting the
-  * previous character read back on the stream. You can use the fail() method to see if unget()
-  * was successful or not. For example, unget() can fail if the current position is at the 
-  * beginning of the stream.
-  *****************************************************************************************/
-  void DemoUngetData()
-  {
-    string guestName;
-    int partySize = 0;
-
-    // Read characters until we find a digit
-    char ch = '\0';
-    cin >> noskipws;
-    while (cin >> ch) {
-      if (isdigit(ch)) {
-        cin.unget();
-        if (cin.fail())
-          cout << "unget() failed" << endl;
-        break;
+      string guestName;
+      int partySize;
+      cout << "Name and number of guests: ";
+      cin >> guestName >> partySize;
+      cout << "Thank you, " << guestName << "." << endl;
+      if (partySize > 10) {
+        cout << "An extra gratuity will apply." << endl;
       }
-      guestName += ch;
-    }
-    // Read partysize, if the stream is not in error state
-    if (cin)
-      cin >> partySize;
-    if (!cin) {
-      cerr << "Error getting party size." << endl;
-      return;
     }
 
-    cout << "Thank you '" << guestName
-      << "', party of " << partySize << endl;
-    if (partySize > 10) {
-      cout << "An extra gratuity will apply." << endl;
-    }
-  }
+    void MyInputErrors()
+    {
+      cout << "Enter numbers on separate lines to add. "
+        << "Use Control+D to finish (Control+Z in Windows)." << endl;
+      int sum = 0;
 
-  // putback() takes the character being placed back on the stream as a parameter
-  void MyPutback()
-  {
-    char cFlag;
-    cin >> cFlag;
-    cin.putback('e');
-  }
-
-  // peek() method allows you to preview the next value that would be returned
-  // if you were to call get().
-  void MyPeek()
-  {
-    string guestName;
-    int partySize = 0;
-
-    // Read characters until we find a digit
-    char ch;
-    cin >> noskipws;
-    while (true) {
-      // 'peek' at next character
-      ch = static_cast<char>(cin.peek());
-      if (!cin)
-        break;
-      if (isdigit(ch)) {
-        // next character will be a digit, so stop the loop
-        break;
+      if (!cin.good()) {
+        cerr << "Standard input is in a bad state!" << endl;
+        return;
       }
-      // next character will be a non-digit, so read it
-      cin >> ch;
-      if (!cin)
-        break;
-      guestName += ch;
-    }
-    // Read partysize, if the stream is not in error state
-    if (cin)
-      cin >> partySize;
-    if (!cin) {
-      cerr << "Error getting party size." << endl;
-      return;
+
+      int number;
+      while (!cin.bad()) {
+        cin >> number;
+        if (cin.good()) {
+          sum += number;
+        }
+        else if (cin.eof()) {
+          break; // Reached end of file
+        }
+        else if (cin.fail()) {
+          // Failure!
+          cin.clear(); // Clear the failure state.
+          string badToken;
+          cin >> badToken; // Consume the bad input.
+          cerr << "WARNING: Bad input encountered: " << badToken << endl;
+        }
+      }
+
+      cout << "The sum is " << sum << endl;
     }
 
-    cout << "Thank you '" << guestName
-      << "', party of " << partySize << endl;
-    if (partySize > 10) {
-      cout << "An extra gratuity will apply." << endl;
-    }
-  }
 
-  void MyGetline()
+    string readName1(istream& stream)
+    {
+      string name;
+      while (stream) { // Or: while (!stream.fail()) {
+        int next = stream.get();//returns the next character in the stream
+        if (!stream || next == std::char_traits<char>::eof())
+          break;
+        name += static_cast<char>(next);// Append character.
+      }
+      return name;
+    }
+
+
+    string readName2(istream& stream)
+    {
+      string name;
+      char next;
+      while (stream.get(next))
+      {
+        name += next;
+      }
+
+      return name;
+    }
+
+    /*****************************************************************************************
+    * A call to unget() causes the stream to back up by one position, essentially putting the
+    * previous character read back on the stream. You can use the fail() method to see if unget()
+    * was successful or not. For example, unget() can fail if the current position is at the
+    * beginning of the stream.
+    *****************************************************************************************/
+    void DemoUngetData()
+    {
+      string guestName;
+      int partySize = 0;
+
+      // Read characters until we find a digit
+      char ch = '\0';
+      cin >> noskipws;
+      while (cin >> ch) {
+        if (isdigit(ch)) {
+          cin.unget();
+          if (cin.fail())
+            cout << "unget() failed" << endl;
+          break;
+        }
+        guestName += ch;
+      }
+      // Read partysize, if the stream is not in error state
+      if (cin)
+        cin >> partySize;
+      if (!cin) {
+        cerr << "Error getting party size." << endl;
+        return;
+      }
+
+      cout << "Thank you '" << guestName
+        << "', party of " << partySize << endl;
+      if (partySize > 10) {
+        cout << "An extra gratuity will apply." << endl;
+      }
+    }
+
+    // putback() takes the character being placed back on the stream as a parameter
+    void MyPutback()
+    {
+      char cFlag;
+      cin >> cFlag;
+      cin.putback('e');
+    }
+
+    // peek() method allows you to preview the next value that would be returned
+    // if you were to call get().
+    void MyPeek()
+    {
+      string guestName;
+      int partySize = 0;
+
+      // Read characters until we find a digit
+      char ch;
+      cin >> noskipws;
+      while (true) {
+        // 'peek' at next character
+        ch = static_cast<char>(cin.peek());
+        if (!cin)
+          break;
+        if (isdigit(ch)) {
+          // next character will be a digit, so stop the loop
+          break;
+        }
+        // next character will be a non-digit, so read it
+        cin >> ch;
+        if (!cin)
+          break;
+        guestName += ch;
+      }
+      // Read partysize, if the stream is not in error state
+      if (cin)
+        cin >> partySize;
+      if (!cin) {
+        cerr << "Error getting party size." << endl;
+        return;
+      }
+
+      cout << "Thank you '" << guestName
+        << "', party of " << partySize << endl;
+      if (partySize > 10) {
+        cout << "An extra gratuity will apply." << endl;
+      }
+    }
+
+    void MyGetline()
+    {
+      const int kBufferSize = 1024;
+      char buffer[kBufferSize] = { 0 };
+      // The getline() method fills a character buffer with a line of data
+      // up to the specified size.
+      cin.getline(buffer, kBufferSize);
+
+      cout << "***" << buffer << "***" << endl;
+
+      // std::getline doesn't require you to specify the size of the buffer
+      string myString;
+      std::getline(cin, myString);
+      cout << "***" << myString << "***" << endl;
+    }
+
+  } // End of namespace DemoInput
+
+
+  namespace InOutObject
   {
-    const int kBufferSize = 1024;
-    char buffer[kBufferSize] = { 0 };
-    // The getline() method fills a character buffer with a line of data
-    // up to the specified size.
-    cin.getline(buffer, kBufferSize);
+    class Muffin
+    {
+    public:
+      virtual ~Muffin() = default;
 
-    cout << "***" << buffer << "***" << endl;
+      string_view getDescription() const;
+      void setDescription(string_view description);
 
-    // std::getline doesn't require you to specify the size of the buffer
-    string myString;
-    std::getline(cin, myString);
-    cout << "***" << myString << "***" << endl;
+      int getSize() const;
+      void setSize(int size);
+
+      bool hasChocolateChips() const;
+      void setHasChocolateChips(bool hasChips);
+
+      void output() const;
+
+    private:
+      string mDescription;
+      int mSize = 0;
+      bool mHasChocolateChips = false;
+    };
+
+    string_view Muffin::getDescription() const { return mDescription; }
+    void Muffin::setDescription(string_view description) { mDescription = description; }
+
+    int Muffin::getSize() const { return mSize; }
+    void Muffin::setSize(int size) { mSize = size; }
+
+    bool Muffin::hasChocolateChips() const { return mHasChocolateChips; }
+    void Muffin::setHasChocolateChips(bool hasChips) { mHasChocolateChips = hasChips; }
+
+    void Muffin::output() const
+    {
+      printf("%s, Size is %d, %s\n", getDescription().data(), getSize(),
+        (hasChocolateChips() ? "has chips" : "no chips"));
+    }
+
+    Muffin createMuffin(istringstream& stream)
+    {
+      Muffin muffin;
+      // Assume data is properly formatted:
+      // Description size chips
+
+      string description;
+      int size;
+      bool hasChips;
+
+      // Read all three values. Note that chips is represented
+      // by the strings "true" and "false"
+      stream >> description >> size >> boolalpha >> hasChips;
+      if (stream) { // Reading was successful.
+        muffin.setSize(size);
+        muffin.setDescription(description);
+        muffin.setHasChocolateChips(hasChips);
+      }
+
+      return muffin;
+    }
+
   }
 
-} // End of namespace DemoInput
+  void chap13DemoInputOutputWithObjects()
+  {
+    FUNC_INFO;
+    cout << "First, let's create a muffin in code and output it." << endl;
 
-void chap13DemoInputsAndErrors()
-{
+    InOutObject::Muffin myMuffin;
+    myMuffin.setDescription("Giant_Blueberry_Muffin");
+    myMuffin.setSize(42);
+    myMuffin.setHasChocolateChips(false);
+
+    printf("The muffin is ");
+    myMuffin.output();
+    printf(" -- yummy!\n");
+
+
+    cout << "Now we'll create a muffin from a string stream" << endl;
+
+    istringstream instream("My_Muffin 2 true");
+    InOutObject::Muffin m2 = InOutObject::createMuffin(instream);
+    m2.output();
+  }
 }
