9c2d6252f7c1260be4672fc3b110c5c963911804
phenix wutao <phenixwutao@gmail.com>
2018-04-28 23:29:47 +0100

    324 - chap13 Demo Inputs And Errors

--- DemoCppNewStandard/ProfCpp4thCh13/ProfCpp4thCh13.cpp
+++ DemoCppNewStandard/ProfCpp4thCh13/ProfCpp4thCh13.cpp
@@ -137,3 +137,191 @@ namespace chap13
     cout << "This should be '1.2346': " << 1.23456789 << endl;
   }
 }
+
+namespace DemoInput
+{
+  void MyInput()
+  {
+    string userInput;
+    cin >> userInput;
+    cout << "User input was " << userInput << endl;
+
+    int intInput;
+    cin >> intInput;
+    cout << "User input was " << intInput << endl;
+
+    string guestName;
+    int partySize;
+    cout << "Name and number of guests: ";
+    cin >> guestName >> partySize;
+    cout << "Thank you, " << guestName << "." << endl;
+    if (partySize > 10) {
+      cout << "An extra gratuity will apply." << endl;
+    }
+  }
+
+  void MyInputErrors()
+  {
+    cout << "Enter numbers on separate lines to add. "
+      << "Use Control+D to finish (Control+Z in Windows)." << endl;
+    int sum = 0;
+
+    if (!cin.good()) {
+      cerr << "Standard input is in a bad state!" << endl;
+      return;
+    }
+
+    int number;
+    while (!cin.bad()) {
+      cin >> number;
+      if (cin.good()) {
+        sum += number;
+      }
+      else if (cin.eof()) {
+        break; // Reached end of file
+      }
+      else if (cin.fail()) {
+        // Failure!
+        cin.clear(); // Clear the failure state.
+        string badToken;
+        cin >> badToken; // Consume the bad input.
+        cerr << "WARNING: Bad input encountered: " << badToken << endl;
+      }
+    }
+
+    cout << "The sum is " << sum << endl;
+  }
+
+
+  string readName1(istream& stream)
+  {
+    string name;
+    while (stream) { // Or: while (!stream.fail()) {
+      int next = stream.get();//returns the next character in the stream
+      if (!stream || next == std::char_traits<char>::eof())
+        break;
+      name += static_cast<char>(next);// Append character.
+    }
+    return name;
+  }
+
+
+  string readName2(istream& stream)
+  {
+    string name;
+    char next;
+    while (stream.get(next))
+    {
+      name += next;
+    }
+
+    return name;
+  }
+
+  /*****************************************************************************************
+  * A call to unget() causes the stream to back up by one position, essentially putting the
+  * previous character read back on the stream. You can use the fail() method to see if unget()
+  * was successful or not. For example, unget() can fail if the current position is at the 
+  * beginning of the stream.
+  *****************************************************************************************/
+  void DemoUngetData()
+  {
+    string guestName;
+    int partySize = 0;
+
+    // Read characters until we find a digit
+    char ch = '\0';
+    cin >> noskipws;
+    while (cin >> ch) {
+      if (isdigit(ch)) {
+        cin.unget();
+        if (cin.fail())
+          cout << "unget() failed" << endl;
+        break;
+      }
+      guestName += ch;
+    }
+    // Read partysize, if the stream is not in error state
+    if (cin)
+      cin >> partySize;
+    if (!cin) {
+      cerr << "Error getting party size." << endl;
+      return;
+    }
+
+    cout << "Thank you '" << guestName
+      << "', party of " << partySize << endl;
+    if (partySize > 10) {
+      cout << "An extra gratuity will apply." << endl;
+    }
+  }
+
+  // putback() takes the character being placed back on the stream as a parameter
+  void MyPutback()
+  {
+    char cFlag;
+    cin >> cFlag;
+    cin.putback('e');
+  }
+
+  // peek() method allows you to preview the next value that would be returned
+  // if you were to call get().
+  void MyPeek()
+  {
+    string guestName;
+    int partySize = 0;
+
+    // Read characters until we find a digit
+    char ch;
+    cin >> noskipws;
+    while (true) {
+      // 'peek' at next character
+      ch = static_cast<char>(cin.peek());
+      if (!cin)
+        break;
+      if (isdigit(ch)) {
+        // next character will be a digit, so stop the loop
+        break;
+      }
+      // next character will be a non-digit, so read it
+      cin >> ch;
+      if (!cin)
+        break;
+      guestName += ch;
+    }
+    // Read partysize, if the stream is not in error state
+    if (cin)
+      cin >> partySize;
+    if (!cin) {
+      cerr << "Error getting party size." << endl;
+      return;
+    }
+
+    cout << "Thank you '" << guestName
+      << "', party of " << partySize << endl;
+    if (partySize > 10) {
+      cout << "An extra gratuity will apply." << endl;
+    }
+  }
+
+  void MyGetline()
+  {
+    const int kBufferSize = 1024;
+    char buffer[kBufferSize] = { 0 };
+    // The getline() method fills a character buffer with a line of data
+    // up to the specified size.
+    cin.getline(buffer, kBufferSize);
+
+    cout << "***" << buffer << "***" << endl;
+
+    // std::getline doesn't require you to specify the size of the buffer
+    string myString;
+    std::getline(cin, myString);
+    cout << "***" << myString << "***" << endl;
+  }
+
+} // End of namespace DemoInput
+
+void chap13DemoInputsAndErrors()
+{
+}
--- DemoCppNewStandard/ProfCpp4thCh13/ProfCpp4thCh13.h
+++ DemoCppNewStandard/ProfCpp4thCh13/ProfCpp4thCh13.h
@@ -4,4 +4,5 @@ namespace chap13
   void chap13DemoBasicStream();
   void chap13DemoCoutException();
   void chap13DemoManipulator();
+  void chap13DemoInputs();
 }
